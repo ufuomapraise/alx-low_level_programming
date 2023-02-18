270a50151b2be1196606c5303187881cfc314b6b
ufuomapraise <ufuomapraise01@gmail.com>
2023-02-18 14:25:42 -0800

    delteed

---
+++ 0x00-hello_world/1-main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{ printf("Hoberton School \n");
+	return (0);
+
+}
---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{ printf("Hoberton School \n");
+	return (0);
+}
