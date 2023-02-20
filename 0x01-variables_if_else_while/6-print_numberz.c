527c3593e69deeb0ff06e79573dd569329e3c1d7
ufuomapraise <ufuomapraise01@gmail.com>
2023-02-20 06:13:09 -0800

    6_Number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar((a % 10) + '0');
+	putchar('\n');
+	return (0);
+}
