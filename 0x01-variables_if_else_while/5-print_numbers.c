46c7fd4653faf27f1db26ae2fc540e50a1e87561
ufuomapraise <ufuomapraise01@gmail.com>
2023-02-20 05:56:40 -0800

    5_Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0- 10 except 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
