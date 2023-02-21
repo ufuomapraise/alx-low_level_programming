169df71385b2491d16bb3d4f7800243cdff3ed33
ufuomapraise <ufuomapraise01@gmail.com>
2023-02-21 13:27:31 -0800

    Task_0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - to print puchar using the putchar function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+		putchar(c[i]);
+
+	putchar('\n');
+	return (0);
+}
