06fe6287244143ad43151a99453d1b4aaa0ac778
ufuomapraise <ufuomapraise@gmail.com>
2024-04-13 03:30:04 +0100

    hash tables

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,35 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+#endif
